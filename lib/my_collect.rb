caa96934123384cef982185d67ded647576467b6
Brenda Ferrufino <ferrufinob@gmail.com>
2020-08-23 01:02:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
 
+def my_collect(array)
+  collect = []
+  array.each do |item|
+    collect << yield(item)
+end
+collect
+end
+my_collect(["ruby", "javascript", "python"]) do |item|
+  item.capitalize
+end
 
+my_collect(["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]) do |item|
+  item.split(",").first
+end
