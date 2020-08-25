a7a97c46fd6e4ed26f5330f5f6d0a0204efb25eb
Theodore Schafer <theodore.schafer@gmail.com>
2020-08-25 16:12:56 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
 
 
+def my_collect(collection)
+  i = 0
+  empty_array = []
+  while i < empty_array.length
+    binding.pry
+    yield empty_array[i]
+    i = i + 1
+  end
+  return empty_array
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
+my_collect(array) do |name|
+  name.split(" ").first
+end
