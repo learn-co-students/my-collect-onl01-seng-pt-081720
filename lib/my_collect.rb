7ac418778ad0143b2b11aac63950c0188c6df146
Jeremy Wood <js.wood206@gmail.com>
2020-08-26 03:30:04 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+require 'pry'
 
-
+def my_collect(array)
+  return "No block given!" unless block_given?
+  counter = 0
+  return_list = []
+  while counter < array.length
+    return_list <<
+    yield(array[counter])
+    counter +=1
+  end
+  return_list
+end
