1d53195689b29b937e15eaf8a4a40cacc33cc1d4
Jimislimdrix <sethsjjones@gmail.com>
2020-08-27 09:55:10 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+def my_collect(arr)
+  if block_given? 
+    i = 0 
+    collection = []
+
+    while i < arr.length 
+      collection << yield(arr[i])
+      i += 1
+    end
+  else 
+    p "No block given!"
+  end 
+  collection 
+end
 
 
