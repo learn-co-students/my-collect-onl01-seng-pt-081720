d2849788816129366b298f0fc62c91a3c2eb0846
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-30 13:59:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,24 @@
-
-
+def my_collect(array)
+  i = 0
+  collection=[]
+  while i < array.length
+  collection << yield(array[i])
+    i += 1
+  end
+  #end of array
+  collection
+end
+#end of collection
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) { |name| name.split(" "). first }
+def my_collect(array)
+  i = 0
+  collection=[]
+  while i < array.length
+  collection << yield(array[i])
+    i += 1
+  end
+  #end of collecting names 
+  collection
+end
+my_collect(["ruby", "javascript", "python"," objective-c"]) { |lang| lang.upcase}
+#collection has all 
